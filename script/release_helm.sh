b93fefc626cd346b97d5a731432f6e5f164259b2
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2025-09-23 11:39:19 +0200

    ci: Change helm chart name and url

--- script/release_helm.sh
+++ script/release_helm.sh
@@ -29,8 +29,8 @@ cd $rootdir/helm
 
 helm package ./camel-dashboard --version $version --app-version $version
 mv camel-dashboard-*.tgz $targetdir/
-# TODO: create https://github.com/camel-tooling/camel-dashboard-charts similar to https://github.com/hawtio/hawtio-charts
-helm repo index $targetdir --url https://camel-tooling.github.io/camel-dashboard/ --merge $helm_index
+echo "Index helm charts helm chart ${version} ..."
+helm repo index $targetdir --url https://camel-tooling.github.io/camel-dashboard/charts/ --merge $helm_index
 # Required to prevent https://github.com/helm/helm/issues/7363
 mv $targetdir/* $targetdir/../.
 rm -rf $targetdir
