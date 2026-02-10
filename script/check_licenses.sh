db560a8716ebcc07f65373cc7f5dded46a4f3052
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2026-02-10 17:22:28 +0100

    chore: exclude json files from license check

--- script/check_licenses.sh
+++ script/check_licenses.sh
@@ -18,7 +18,7 @@
 location=$(dirname $0)
 rootdir=$location/../
 
-denylist=("zz_generated" "zz_desc_generated" "vendor" "./.mvn/wrapper" "./docs/" "./.idea" "./build/" "./deploy/traits.yaml" "./pom.xml" "./java/")
+denylist=("zz_generated" "zz_desc_generated" "vendor" "./.mvn/wrapper" "./docs/" "./.idea" "./build/" "./deploy/traits.yaml" "./pom.xml" "./java/", "*.json")
 
 cd $rootdir
 go build ./cmd/util/license-check/
